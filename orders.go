ca31d6659fa0297daafe2b7e6b0020e7065aa6bc
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2022-05-12 15:42:13 +0200

    Add orders structs

--- orders.go
+++ orders.go
@@ -10,3 +10,57 @@
 //********************************************************************************************************************//
 
 package gotradebyte
+
+// OrdersBody is to decode xml data
+type OrdersBody struct {
+	Order []OrdersBodyOrder `xml:"ORDER"`
+}
+
+type OrdersBodyOrder struct {
+	OrderData OrdersBodyOrderData `xml:"ORDER_DATA"`
+	SellTo    OrdersBodySellTo    `xml:"SELL_TO"`
+	ShipTo    OrdersBodyShipTo    `xml:"SHIP_TO"`
+	Items     []OrdersBodyItem    `xml:"ITEMS"`
+}
+
+type OrdersBodyOrderData struct {
+	OrderDate       string `xml:"ORDER_DATE"`
+	ChannelSign     string `xml:"CHANNEL_SIGN"`
+	ChannelId       int    `xml:"CHANNEL_ID"`
+	ChannelNo       string `xml:"CHANNEL_NO"`
+	Approved        int    `xml:"APPROVED"`
+	ItemCount       int    `xml:"ITEM_COUNT"`
+	TotalItemAmount string `xml:"TOTAL_ITEM_AMOUNT"`
+}
+
+type OrdersBodySellTo struct {
+	ChannelNo    int    `xml:"CHANNEL_NO"`
+	FirstName    string `xml:"FIRSTNAME"`
+	LastName     string `xml:"LASTNAME"`
+	StreetNo     string `xml:"STREET_NO"`
+	Zip          string `xml:"ZIP"`
+	City         string `xml:"CITY"`
+	Country      string `xml:"COUNTRY"`
+	PhonePrivate string `xml:"PHONE_PRIVATE"`
+	Email        string `xml:"EMAIL"`
+}
+
+type OrdersBodyShipTo struct {
+	ChannelNo int    `xml:"CHANNEL_NO"`
+	FirstName string `xml:"FIRSTNAME"`
+	LastName  string `xml:"LASTNAME"`
+	StreetNo  string `xml:"STREET_NO"`
+	Zip       string `xml:"ZIP"`
+	City      string `xml:"CITY"`
+	Country   string `xml:"COUNTRY"`
+}
+
+type OrdersBodyItem struct {
+	ChannelId   int     `xml:"CHANNEL_ID"`
+	Sku         string  `xml:"SKU"`
+	ChannelSku  string  `xml:"CHANNEL_SKU"`
+	Ean         string  `xml:"EAN"`
+	Quantity    int     `xml:"QUANTITY"`
+	BillingText string  `xml:"BILLING_TEXT"`
+	ItemPrice   float64 `xml:"ITEM_PRICE"`
+}
