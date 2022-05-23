7a1445c3102e0232fc8ccc3c3b0528690a8556d2
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2022-05-23 10:18:42 +0200

    Update structs

--- orders.go
+++ orders.go
@@ -25,6 +25,7 @@ type OrdersBodyOrder struct {
 
 type OrdersBodyOrderData struct {
 	OrderDate       string `xml:"ORDER_DATE"`
+	TbId            int    `xml:"TB_ID"`
 	ChannelSign     string `xml:"CHANNEL_SIGN"`
 	ChannelId       string `xml:"CHANNEL_ID"`
 	ChannelNo       string `xml:"CHANNEL_NO"`
@@ -34,25 +35,29 @@ type OrdersBodyOrderData struct {
 }
 
 type OrdersBodySellTo struct {
-	ChannelNo    int    `xml:"CHANNEL_NO"`
-	FirstName    string `xml:"FIRSTNAME"`
-	LastName     string `xml:"LASTNAME"`
-	StreetNo     string `xml:"STREET_NO"`
-	Zip          string `xml:"ZIP"`
-	City         string `xml:"CITY"`
-	Country      string `xml:"COUNTRY"`
-	PhonePrivate string `xml:"PHONE_PRIVATE"`
-	Email        string `xml:"EMAIL"`
+	TbId            int    `xml:"TB_ID"`
+	ChannelNo       int    `xml:"CHANNEL_NO"`
+	FirstName       string `xml:"FIRSTNAME"`
+	LastName        string `xml:"LASTNAME"`
+	StreetNo        string `xml:"STREET_NO"`
+	StreetExtension string `xml:"STREET_EXTENSION"`
+	Zip             string `xml:"ZIP"`
+	City            string `xml:"CITY"`
+	Country         string `xml:"COUNTRY"`
+	PhonePrivate    string `xml:"PHONE_PRIVATE"`
+	Email           string `xml:"EMAIL"`
 }
 
 type OrdersBodyShipTo struct {
-	ChannelNo int    `xml:"CHANNEL_NO"`
-	FirstName string `xml:"FIRSTNAME"`
-	LastName  string `xml:"LASTNAME"`
-	StreetNo  string `xml:"STREET_NO"`
-	Zip       string `xml:"ZIP"`
-	City      string `xml:"CITY"`
-	Country   string `xml:"COUNTRY"`
+	TbId            int    `xml:"TB_ID"`
+	ChannelNo       int    `xml:"CHANNEL_NO"`
+	FirstName       string `xml:"FIRSTNAME"`
+	LastName        string `xml:"LASTNAME"`
+	StreetNo        string `xml:"STREET_NO"`
+	StreetExtension string `xml:"STREET_EXTENSION"`
+	Zip             string `xml:"ZIP"`
+	City            string `xml:"CITY"`
+	Country         string `xml:"COUNTRY"`
 }
 
 type OrdersBodyItem struct {
