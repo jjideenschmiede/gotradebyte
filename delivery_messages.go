3974f10fa4bf8dbf2bc3044d1615391663ef6e61
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2022-07-04 10:26:15 +0200

    Update structs

--- delivery_messages.go
+++ delivery_messages.go
@@ -11,27 +11,23 @@
 
 package gotradebyte
 
-import "encoding/xml"
-
 // DeliveryMessagesBody is to encode xml data
 type DeliveryMessagesBody struct {
 	MessagesList []DeliveryMessagesBodyMessagesList `xml:"MESSAGES_LIST"`
 }
 
 type DeliveryMessagesBodyMessagesList struct {
-	XmlName xml.Name                    `xml:"MESSAGES_LIST"`
 	Message DeliveryMessagesBodyMessage `xml:"MESSAGE"`
 }
 
 type DeliveryMessagesBodyMessage struct {
-	XmlName           xml.Name `xml:"MESSAGE"`
-	MessageType       string   `xml:"MESSAGE_TYPE"`
-	TbOrderId         int      `xml:"TB_ORDER_ID"`
-	TbOrderItemId     int      `xml:"TB_ORDER_ITEM_ID"`
-	Sku               string   `xml:"SKU"`
-	ChannelSign       string   `xml:"CHANNEL_SIGN"`
-	ChannelOrderId    string   `xml:"CHANNEL_ORDER_ID"`
-	Quantity          int      `xml:"QUANTITY"`
-	CarrierParcelType string   `xml:"CARRIER_PARCEL_TYPE"`
-	IdCode            string   `xml:"IDCODE"`
+	MessageType       string `xml:"MESSAGE_TYPE"`
+	TbOrderId         int    `xml:"TB_ORDER_ID"`
+	TbOrderItemId     int    `xml:"TB_ORDER_ITEM_ID"`
+	Sku               string `xml:"SKU"`
+	ChannelSign       string `xml:"CHANNEL_SIGN"`
+	ChannelOrderId    string `xml:"CHANNEL_ORDER_ID"`
+	Quantity          int    `xml:"QUANTITY"`
+	CarrierParcelType string `xml:"CARRIER_PARCEL_TYPE"`
+	IdCode            string `xml:"IDCODE"`
 }
